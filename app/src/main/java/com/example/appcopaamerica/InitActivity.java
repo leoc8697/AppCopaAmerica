8a3caf67a5bebc302a47c57b8120d72ec876c27f
hgeraso <hgeraso@unicauca.edu.co>
2019-06-03 17:37:02 -0500

    implementMainActivity

--- app/src/main/java/com/example/appcopaamerica/InitActivity.java
+++ app/src/main/java/com/example/appcopaamerica/InitActivity.java
@@ -3,11 +3,12 @@ package com.example.appcopaamerica;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class InitActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_init);
+        getSupportActionBar().hide();
     }
 }
