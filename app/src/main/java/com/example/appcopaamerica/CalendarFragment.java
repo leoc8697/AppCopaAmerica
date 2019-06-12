d63253316d29aa25c3ffdca9fab63499666021e5
david <leoc@unicauca.edu.co>
2019-06-12 11:16:00 -0500

    Tab Layout created.

---
+++ app/src/main/java/com/example/appcopaamerica/CalendarFragment.java
@@ -0,0 +1,26 @@
+package com.example.appcopaamerica;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class CalendarFragment extends Fragment {
+
+    public CalendarFragment(){
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.calendar_fragment, container, false);
+    }
+
+}
---
+++ app/src/main/java/com/example/appcopaamerica/PageAdapter.java
@@ -0,0 +1,38 @@
+package com.example.appcopaamerica;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+public class PageAdapter extends FragmentStatePagerAdapter {
+    int mNumOfTabs;
+
+    public PageAdapter(FragmentManager fm, int NumOfTabs) {
+        super(fm);
+        this.mNumOfTabs = NumOfTabs;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+
+        switch (position) {
+            case 0:
+                CalendarFragment calendar_frag = new CalendarFragment();
+                return calendar_frag;
+            case 1:
+                PositionsFragment positions_frag = new PositionsFragment();
+                return positions_frag;
+            case 2:
+                TeamsFragment teams_frag = new TeamsFragment();
+                return teams_frag;
+            default:
+                CalendarFragment calendar_fragm = new CalendarFragment();
+                return calendar_fragm;
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return mNumOfTabs;
+    }
+}
