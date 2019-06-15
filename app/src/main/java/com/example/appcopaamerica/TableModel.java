7723d424b41e69b196f18882e57515b6d962d9cd
david <leoc@unicauca.edu.co>
2019-06-15 00:32:50 -0500

    Table Model created.

---
+++ app/src/main/java/com/example/appcopaamerica/TableModel.java
@@ -0,0 +1,283 @@
+package com.example.appcopaamerica;
+
+public class TableModel {
+
+    private String  nameGroup, infoGroup, position1, position2, position3, position4,
+                    namePosition1, namePosition2, namePosition3, namePosition4,
+                    pj1, pj2, pj3, pj4, goles1, goles2, goles3, goles4,
+                    difGol1, difGol2, difGol3, difGol4, pts1, pts2, pts3, pts4;
+    private int imgPos1, imgPos2, imgPos3, imgPos4;
+
+    public TableModel(String nameGroup, String infoGroup, String position1, String position2, String position3, String position4, String namePosition1, String namePosition2, String namePosition3, String namePosition4, String pj1, String pj2, String pj3, String pj4, String goles1, String goles2, String goles3, String goles4, String difGol1, String difGol2, String difGol3, String difGol4, String pts1, String pts2, String pts3, String pts4, int imgPos1, int imgPos2, int imgPos3, int imgPos4) {
+        this.nameGroup = nameGroup;
+        this.infoGroup = infoGroup;
+        this.position1 = position1;
+        this.position2 = position2;
+        this.position3 = position3;
+        this.position4 = position4;
+        this.namePosition1 = namePosition1;
+        this.namePosition2 = namePosition2;
+        this.namePosition3 = namePosition3;
+        this.namePosition4 = namePosition4;
+        this.pj1 = pj1;
+        this.pj2 = pj2;
+        this.pj3 = pj3;
+        this.pj4 = pj4;
+        this.goles1 = goles1;
+        this.goles2 = goles2;
+        this.goles3 = goles3;
+        this.goles4 = goles4;
+        this.difGol1 = difGol1;
+        this.difGol2 = difGol2;
+        this.difGol3 = difGol3;
+        this.difGol4 = difGol4;
+        this.pts1 = pts1;
+        this.pts2 = pts2;
+        this.pts3 = pts3;
+        this.pts4 = pts4;
+        this.imgPos1 = imgPos1;
+        this.imgPos2 = imgPos2;
+        this.imgPos3 = imgPos3;
+        this.imgPos4 = imgPos4;
+    }
+
+    public String getNameGroup() {
+        return nameGroup;
+    }
+
+    public void setNameGroup(String nameGroup) {
+        this.nameGroup = nameGroup;
+    }
+
+    public String getInfoGroup() {
+        return infoGroup;
+    }
+
+    public void setInfoGroup(String infoGroup) {
+        this.infoGroup = infoGroup;
+    }
+
+    public String getPosition1() {
+        return position1;
+    }
+
+    public void setPosition1(String position1) {
+        this.position1 = position1;
+    }
+
+    public String getPosition2() {
+        return position2;
+    }
+
+    public void setPosition2(String position2) {
+        this.position2 = position2;
+    }
+
+    public String getPosition3() {
+        return position3;
+    }
+
+    public void setPosition3(String position3) {
+        this.position3 = position3;
+    }
+
+    public String getPosition4() {
+        return position4;
+    }
+
+    public void setPosition4(String position4) {
+        this.position4 = position4;
+    }
+
+    public String getNamePosition1() {
+        return namePosition1;
+    }
+
+    public void setNamePosition1(String namePosition1) {
+        this.namePosition1 = namePosition1;
+    }
+
+    public String getNamePosition2() {
+        return namePosition2;
+    }
+
+    public void setNamePosition2(String namePosition2) {
+        this.namePosition2 = namePosition2;
+    }
+
+    public String getNamePosition3() {
+        return namePosition3;
+    }
+
+    public void setNamePosition3(String namePosition3) {
+        this.namePosition3 = namePosition3;
+    }
+
+    public String getNamePosition4() {
+        return namePosition4;
+    }
+
+    public void setNamePosition4(String namePosition4) {
+        this.namePosition4 = namePosition4;
+    }
+
+    public String getPj1() {
+        return pj1;
+    }
+
+    public void setPj1(String pj1) {
+        this.pj1 = pj1;
+    }
+
+    public String getPj2() {
+        return pj2;
+    }
+
+    public void setPj2(String pj2) {
+        this.pj2 = pj2;
+    }
+
+    public String getPj3() {
+        return pj3;
+    }
+
+    public void setPj3(String pj3) {
+        this.pj3 = pj3;
+    }
+
+    public String getPj4() {
+        return pj4;
+    }
+
+    public void setPj4(String pj4) {
+        this.pj4 = pj4;
+    }
+
+    public String getGoles1() {
+        return goles1;
+    }
+
+    public void setGoles1(String goles1) {
+        this.goles1 = goles1;
+    }
+
+    public String getGoles2() {
+        return goles2;
+    }
+
+    public void setGoles2(String goles2) {
+        this.goles2 = goles2;
+    }
+
+    public String getGoles3() {
+        return goles3;
+    }
+
+    public void setGoles3(String goles3) {
+        this.goles3 = goles3;
+    }
+
+    public String getGoles4() {
+        return goles4;
+    }
+
+    public void setGoles4(String goles4) {
+        this.goles4 = goles4;
+    }
+
+    public String getDifGol1() {
+        return difGol1;
+    }
+
+    public void setDifGol1(String difGol1) {
+        this.difGol1 = difGol1;
+    }
+
+    public String getDifGol2() {
+        return difGol2;
+    }
+
+    public void setDifGol2(String difGol2) {
+        this.difGol2 = difGol2;
+    }
+
+    public String getDifGol3() {
+        return difGol3;
+    }
+
+    public void setDifGol3(String difGol3) {
+        this.difGol3 = difGol3;
+    }
+
+    public String getDifGol4() {
+        return difGol4;
+    }
+
+    public void setDifGol4(String difGol4) {
+        this.difGol4 = difGol4;
+    }
+
+    public String getPts1() {
+        return pts1;
+    }
+
+    public void setPts1(String pts1) {
+        this.pts1 = pts1;
+    }
+
+    public String getPts2() {
+        return pts2;
+    }
+
+    public void setPts2(String pts2) {
+        this.pts2 = pts2;
+    }
+
+    public String getPts3() {
+        return pts3;
+    }
+
+    public void setPts3(String pts3) {
+        this.pts3 = pts3;
+    }
+
+    public String getPts4() {
+        return pts4;
+    }
+
+    public void setPts4(String pts4) {
+        this.pts4 = pts4;
+    }
+
+    public int getImgPos1() {
+        return imgPos1;
+    }
+
+    public void setImgPos1(int imgPos1) {
+        this.imgPos1 = imgPos1;
+    }
+
+    public int getImgPos2() {
+        return imgPos2;
+    }
+
+    public void setImgPos2(int imgPos2) {
+        this.imgPos2 = imgPos2;
+    }
+
+    public int getImgPos3() {
+        return imgPos3;
+    }
+
+    public void setImgPos3(int imgPos3) {
+        this.imgPos3 = imgPos3;
+    }
+
+    public int getImgPos4() {
+        return imgPos4;
+    }
+
+    public void setImgPos4(int imgPos4) {
+        this.imgPos4 = imgPos4;
+    }
+}
